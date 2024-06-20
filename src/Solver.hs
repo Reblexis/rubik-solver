9500c450e9edfce0f8f5ee955f3b3b88db26b5ad
Viktor Cihal <viktor.cihal@gmail.com>
2024-06-20 18:42:22 +0200

    Add evaluation

--- src/Solver.hs
+++ src/Solver.hs
@@ -1,7 +1,7 @@
 {-
 Rubik's cube is represented in the following way:
 A tuple of tuples with fixed sizes, so data = (BACK, TOP, LEFT, FRONT, RIGHT, BOTTOM)
-and BACK,TOP,..,BOTTOM = (UL, UM, UR, ML, MM, MR, BL, BM, BB) if we rotate to it from the front
+and BACK,TOP,..,BOTTOM = (UL, UM, tr, ML, MM, MR, BL, BM, BB) if we rotate to it from the front
     side via the shortest path (and to the back side we rotate through top side).
     And colors are represented as integers via the following mapping: Y,B,O,W,R,G = 0,1,2,3,4,5
 
@@ -10,8 +10,12 @@ and BACK,TOP,..,BOTTOM = (UL, UM, UR, ML, MM, MR, BL, BM, BB) if we rotate to it
 -}
 -- Define the Side and Cube types
 -- Define the Side and Cube types using records for clearer field access
+
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+
 data Side = Side {
-    ul :: Int, um :: Int, ur :: Int,
+    tl :: Int, tm :: Int, tr :: Int,
     ml :: Int, mm :: Int, mr :: Int,
     bl :: Int, bm :: Int, br :: Int
 } deriving Show
@@ -28,9 +32,9 @@ data Cube = Cube {
 -- Function to rotate a Side clockwise
 rotateSideC :: Side -> Side
 rotateSideC s = Side {
-    ul = bl s, um = ml s, ur = ul s,
-    ml = bm s, mm = mm s, mr = um s,
-    bl = br s, bm = mr s, br = ur s
+    tl = bl s, tm = ml s, tr = tl s,
+    ml = bm s, mm = mm s, mr = tm s,
+    bl = br s, bm = mr s, br = tr s
 }
 
 -- Function to rotate a Side counterclockwise
@@ -40,7 +44,7 @@ rotateSideCC side = rotateSideC $ rotateSideC $ rotateSideC side
 -- Function to modify the right column from another side's right column
 changeRightFrom :: Side -> Side -> Side
 changeRightFrom s1 s2 = Side {
-    ul = ul s1, um = um s1, ur = ur s2,
+    tl = tl s1, tm = tm s1, tr = ur s2,
     ml = ml s1, mm = mm s1, mr = mr s2,
     bl = bl s1, bm = bm s1, br = br s2
 }
@@ -59,7 +63,7 @@ rMove c = Cube {
 -- Modify the middle column from another side's middle column
 changeMiddleFrom :: Side -> Side -> Side
 changeMiddleFrom s1 s2 = Side {
-    ul = ul s1, um = um s2, ur = ur s1,
+    tl = tl s1, tm = tm s2, tr = ur s1,
     ml = ml s1, mm = mm s2, mr = mr s1,
     bl = bl s1, bm = bm s2, br = br s1
 }
@@ -114,13 +118,30 @@ bMove c = (yRotation) $ rMove $ (prime yRotation) c
 
 
 -- Check if there is cross in the middle matching middle points of the other sides
-isCross :: Cube -> Bool
-isCross c = all (== mm (front c)) [um (front c), ml (front c), mr (front c), bm (front c)] && bm (top c) == mm (top c) 
-    && mr(left c) == mm (left c) && um (bottom c) == mm (bottom c) && ml (right c) == mm (right c)
+isCrossFront :: Cube -> Bool
+isCrossFront c = all (== mm (front c)) [tm (front c), ml (front c), mr (front c), bm (front c)] && bm (top c) == mm (top c) 
+    && mr(left c) == mm (left c) && tm (bottom c) == mm (bottom c) && ml (right c) == mm (right c)
+
+boolToInt :: Bool -> Int
+boolToInt True = 1
+boolToInt False = 0
+
+countTrues :: [Bool] -> Int
+countTrues = stm . map boolToInt
+
+"""
+Assigns score to the cube
+"""
+evaluate :: Cube -> Int
+evaluate c 
+    | isCross c = 5+countTrues [tl (front c) == mm (front c) && bl (top c) == bm (top c) && tr(left c) == mr(left c), ml (top c) == mm (top c) && tm (left c) == mm(left c),
+                                tr (front c) == mm (front c) && br (top c) == bm (top c) && tl(right c) == ml(right c), mr (top c) == mm (top c) && tm (right c) == mm (right c)]
+    | otherwise = countTrues [all (== mm (front c)) [tm (front c), ml (front c), mr (front c), bm (front c)], bm (top c) == mm (top c) 
+    ,mr(left c) == mm (left c), tm (bottom c) == mm (bottom c), ml (right c) == mm (right c)]
 
 -- Check if a side is one color
 isOneColor :: Side -> Bool
-isOneColor s = all (== ul s) [um s, ur s, ml s, mm s, mr s, bl s, bm s, br s]
+isOneColor s = all (== tl s) [tm s, tr s, ml s, mm s, mr s, bl s, bm s, br s]
 
 -- Check if the entire cube is solved
 isSolved :: Cube -> Bool
@@ -129,31 +150,31 @@ isSolved c = all isOneColor [back c, top c, left c, front c, right c, bottom c]
 -- A representation of a solved cube
 solvedCube :: Cube
 solvedCube = Cube {
-    back = Side {ul = 0, um = 0, ur = 0, ml = 0, mm = 0, mr = 0, bl = 0, bm = 0, br = 0},
-    top = Side {ul = 1, um = 1, ur = 1, ml = 1, mm = 1, mr = 1, bl = 1, bm = 1, br = 1},
-    left = Side {ul = 2, um = 2, ur = 2, ml = 2, mm = 2, mr = 2, bl = 2, bm = 2, br = 2},
-    front = Side {ul = 3, um = 3, ur = 3, ml = 3, mm = 3, mr = 3, bl = 3, bm = 3, br = 3},
-    right = Side {ul = 4, um = 4, ur = 4, ml = 4, mm = 4, mr = 4, bl = 4, bm = 4, br = 4},
-    bottom = Side {ul = 5, um = 5, ur = 5, ml = 5, mm = 5, mr = 5, bl = 5, bm = 5, br = 5}
+    back = Side {tl = 0, tm = 0, tr = 0, ml = 0, mm = 0, mr = 0, bl = 0, bm = 0, br = 0},
+    top = Side {tl = 1, tm = 1, tr = 1, ml = 1, mm = 1, mr = 1, bl = 1, bm = 1, br = 1},
+    left = Side {tl = 2, tm = 2, tr = 2, ml = 2, mm = 2, mr = 2, bl = 2, bm = 2, br = 2},
+    front = Side {tl = 3, tm = 3, tr = 3, ml = 3, mm = 3, mr = 3, bl = 3, bm = 3, br = 3},
+    right = Side {tl = 4, tm = 4, tr = 4, ml = 4, mm = 4, mr = 4, bl = 4, bm = 4, br = 4},
+    bottom = Side {tl = 5, tm = 5, tr = 5, ml = 5, mm = 5, mr = 5, bl = 5, bm = 5, br = 5}
 }
 
--- Current cube state, remaining depth, accumulated moves, and a solution
-findMoves :: Cube -> Int -> [String] -> [String]
-findMoves cube 0 moves 
-    | isCross cube = moves
-    | otherwise = []
-findMoves cube depth moves
-    | isCross cube = moves
-    | otherwise = 
-        let r = findMoves (rMove cube) (depth - 1) (moves ++ ["R"])
-            m = findMoves (mMove cube) (depth - 1) (moves ++ ["M"])
-            c = findMoves (zRotation cube) (depth - 1) (moves ++ ["RC"])
-        in if null r then if null m then c else m else r
+selectBest :: [([String], Int)] -> ([String], Int)
+selectBest = maximumBy (comparing snd) tuples
 
--- Current cube state, maximum depth, current depth, and a solution
-findSolutionIterative :: Cube -> Int -> Int -> [String]
+-- Current cube state, remaining depth, accumulated moves, and a solution
+findMoves :: Cube -> Int -> [String] -> ([String], Int)
+findMoves cube depth limit moves
+    | (depth == limit) = (moves, evaluate cube)
+    | otherwise = 
+            selectBest [(moves, evaluate cube),
+            findMoves (rMove cube) (depth - 1) (moves ++ ["R"])
+            ,findMoves (mMove cube) (depth - 1) (moves ++ ["M"])
+            ,findMoves (zRotation cube) (depth - 1) (moves ++ ["RC"])]
+
+-- Current cube state, maximtm depth, current depth, and a solution
+findSolutionIterative :: Cube -> Int -> Int -> ([String], Int)
 findSolutionIterative cube maxDepth depth
-    | depth > maxDepth = []
+    | depth > maxDepth = ()
     | otherwise = 
         let moves = findMoves cube depth []
         in if null moves then findSolutionIterative cube maxDepth (depth + 1) else moves
